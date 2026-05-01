53762eee71a3d39f388f11db7050373286ee0413
levent-istemi <leventistemi5@gmail.com>
2026-05-01 21:56:15 +0300

    Create sorular.js

---
+++ sorular.js
@@ -0,0 +1,14 @@
+const SORU_BANKASI = [
+    {
+        "ders_adi": "İŞ SAĞLIĞI VE GÜVENLİĞİ UYGULAMASI",
+        "test_adi": "Final - Test 1",
+        "soru_metni": "Radyasyon yoğunluğunun en yüksek olduğu irtifa düzeyi aşağıdakilerden hangisinde verilmiştir?",
+        "secenek_a": "5 km",
+        "secenek_b": "7 km",
+        "secenek_c": "18 km",
+        "secenek_d": "15 km",
+        "secenek_e": "10 km",
+        "dogru_cevap": "Qw==" 
+    },
+    // Diğer sorular da bu şekilde aşağıya doğru devam edecek...
+];
